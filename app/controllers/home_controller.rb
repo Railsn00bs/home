625ce31b08e94e1809a1e76aeb7a4e29911d09f3
Nick Johnstone <ncwjohnstone@gmail.com>
2015-09-06 21:56:27 +1200

    Move the no events scheduled sad cat logic into the view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,5 @@ class HomeController < ApplicationController
   def index
     @classifier = AppropriateEventEmoji.new
     @events = Event.where('start_at > ?', Time.now - 1.week)
-    if @events.none?
-      @events = [
-        OpenStruct.new(:name => 'None 😿')
-      ]
-    end
   end
 end
